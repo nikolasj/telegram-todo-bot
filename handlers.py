224ce021a2a895aa0e70285bac5daae768ff26e2
Nikolay Yusev <n.yusev@bi.zone>
2020-05-16 10:34:01 +0700

    Проверка доступа к телеграмм

--- handlers.py
+++ handlers.py
@@ -1,11 +1,13 @@
-from aiogram.types import (Message)
+from main import bot, dp
+from aiogram.types import Message
+from config import ADMIN_ID
 
-import db
-from load_all import dp
 
-db = db.DBCommands()
+async def send_to_admin(*args):
+    await bot.send_message(chat_id=ADMIN_ID, text="Бот запущен")
 
 
 @dp.message_handler()
-async def other_echo(message: Message):
-    await message.answer(message.text)
+async def echo(message: Message):
+    text = f"Привет, ты написал: {message.text}"
+    await message.reply(text=text)
---
+++ main.py
@@ -0,0 +1,17 @@
+import asyncio
+
+from aiogram import Bot, Dispatcher, executor
+from config import BOT_TOKEN
+
+# loop = asyncio.get_event_loop()
+# Поток нам не нужен, т.к. он и так создается в диспатчере.
+bot = Bot(BOT_TOKEN, parse_mode="HTML")
+dp = Dispatcher(bot)
+
+import urllib3.contrib.pyopenssl
+urllib3.contrib.pyopenssl.inject_into_urllib3()
+
+if __name__ == '__main__':
+    from handlers import dp, send_to_admin
+
+    executor.start_polling(dp, on_startup=send_to_admin)
